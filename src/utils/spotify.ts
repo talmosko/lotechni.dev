e5b533ae11ee311ad8f5aff303e6282bb6d01117
talmosko <tal.moskovich@gmail.com>
2024-12-22 18:39:01 +0200

    decrease tab

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -4,53 +4,53 @@ import { EpisodesResponseSchema } from '../types/spotifyEpisodes'
 import showMock from '../types/show.mock'
 
 export async function getAccessToken(clientId: string, clientSecret: string) {
-	const response = await fetch('https://accounts.spotify.com/api/token', {
-		method: 'POST',
-		headers: {
-			Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: 'grant_type=client_credentials',
-	})
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'grant_type=client_credentials',
+  })
 
-	const data = await response.json()
-	return data.access_token
+  const data = await response.json()
+  return data.access_token
 }
 
 export async function fetchEpisodes(showId: string) {
-	// try {
-	//   const accessToken = await getAccessToken(
-	//     import.meta.env.SPOTIFY_CLIENT_ID,
-	//     import.meta.env.SPOTIFY_CLIENT_SECRET
-	//   );
-	//   const response = await fetch(
-	//     `https://api.spotify.com/v1/shows/${showId}?limit=50`,
-	//     {
-	//       method: "GET",
-	//       headers: {
-	//         Authorization: `Bearer ${accessToken}`,
-	//       },
-	//     }
-	//   );
+  // try {
+  //   const accessToken = await getAccessToken(
+  //     import.meta.env.SPOTIFY_CLIENT_ID,
+  //     import.meta.env.SPOTIFY_CLIENT_SECRET
+  //   );
+  //   const response = await fetch(
+  //     `https://api.spotify.com/v1/shows/${showId}?limit=50`,
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     }
+  //   );
 
-	//   const data = await response.json();
-	//   console.log(data);
-	//   const show = EpisodesResponseSchema.parse(data);
-	//   return show;
-	// } catch (error) {
-	//   console.log(error);
-	//   return EpisodesResponseSchema.parse(showMock);
-	// }
-	return EpisodesResponseSchema.parse(showMock)
+  //   const data = await response.json();
+  //   console.log(data);
+  //   const show = EpisodesResponseSchema.parse(data);
+  //   return show;
+  // } catch (error) {
+  //   console.log(error);
+  //   return EpisodesResponseSchema.parse(showMock);
+  // }
+  return EpisodesResponseSchema.parse(showMock)
 }
 
 const getRssFeed = async () => {
-	const res = await fetch('https://anchor.fm/s/f01f6814/podcast/rss')
-	const xml = await res.text()
-	const parser = new XMLParser({
-		ignoreAttributes: false,
-	})
-	const json = parser.parse(xml)
-	const rssFeed = PodcastRSSFeed.parse(json)
-	return rssFeed
+  const res = await fetch('https://anchor.fm/s/f01f6814/podcast/rss')
+  const xml = await res.text()
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+  })
+  const json = parser.parse(xml)
+  const rssFeed = PodcastRSSFeed.parse(json)
+  return rssFeed
 }
