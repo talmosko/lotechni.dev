9c8bdccf12c38ae3017fbc8fc7805741d7616467
MeirJC <meir84@gmail.com>
2024-12-22 17:38:41 +0200

    eslint with air-bnb config settings

--- src/types/rssFeed.ts
+++ src/types/rssFeed.ts
@@ -1,92 +1,92 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const Guid = z.union([
-  z.object({
-    "@_isPermaLink": z.string(),
-    "#text": z.string(),
-  }),
-  z.string(),
-]);
+	z.object({
+		'@_isPermaLink': z.string(),
+		'#text': z.string(),
+	}),
+	z.string(),
+])
 
 const Enclosure = z.object({
-  "@_url": z.string(),
-  "@_length": z.string(),
-  "@_type": z.string(),
-});
+	'@_url': z.string(),
+	'@_length': z.string(),
+	'@_type': z.string(),
+})
 
 const ItunesImage = z
-  .object({
-    "@_href": z.string(),
-  })
-  .optional();
+	.object({
+		'@_href': z.string(),
+	})
+	.optional()
 
 const ItunesCategorySingle = z.object({
-  "@_text": z.string().optional(),
-});
+	'@_text': z.string().optional(),
+})
 
-const ItunesCategory = z.union([z.array(ItunesCategorySingle), ItunesCategorySingle]);
+const ItunesCategory = z.union([z.array(ItunesCategorySingle), ItunesCategorySingle])
 
 const ItunesOwner = z.object({
-  "itunes:name": z.string(),
-  "itunes:email": z.string(),
-});
+	'itunes:name': z.string(),
+	'itunes:email': z.string(),
+})
 
 const AtomLinkSingle = z.object({
-  "@_href": z.string(),
-  "@_rel": z.string(),
-  "@_type": z.optional(z.string()),
-});
+	'@_href': z.string(),
+	'@_rel': z.string(),
+	'@_type': z.optional(z.string()),
+})
 
-const AtomLink = z.union([z.array(AtomLinkSingle), AtomLinkSingle]);
+const AtomLink = z.union([z.array(AtomLinkSingle), AtomLinkSingle])
 
 const Image = z.object({
-  url: z.string(),
-  title: z.string(),
-  link: z.string(),
-});
+	url: z.string(),
+	title: z.string(),
+	link: z.string(),
+})
 
 const Item = z.object({
-  title: z.string(),
-  description: z.string(),
-  link: z.string().optional(),
-  guid: Guid,
-  "dc:creator": z.string().optional(),
-  pubDate: z.string(),
-  enclosure: Enclosure,
-  "itunes:summary": z.string().optional(),
-  "itunes:explicit": z.union([z.string(), z.boolean()]),
-  "itunes:duration": z.union([z.string(), z.number().transform((n) => n.toString())]),
-  "itunes:image": ItunesImage,
-  "itunes:episodeType": z.string().optional(),
-  "itunes:season": z.union([z.string(), z.number().transform((n) => n.toString())]).optional(),
-  "itunes:episode": z.union([z.string(), z.number().transform((n) => n.toString())]).optional(),
-});
+	title: z.string(),
+	description: z.string(),
+	link: z.string().optional(),
+	guid: Guid,
+	'dc:creator': z.string().optional(),
+	pubDate: z.string(),
+	enclosure: Enclosure,
+	'itunes:summary': z.string().optional(),
+	'itunes:explicit': z.union([z.string(), z.boolean()]),
+	'itunes:duration': z.union([z.string(), z.number().transform((n) => n.toString())]),
+	'itunes:image': ItunesImage,
+	'itunes:episodeType': z.string().optional(),
+	'itunes:season': z.union([z.string(), z.number().transform((n) => n.toString())]).optional(),
+	'itunes:episode': z.union([z.string(), z.number().transform((n) => n.toString())]).optional(),
+})
 
 const Channel = z.object({
-  title: z.string(),
-  description: z.string(),
-  link: z.string(),
-  image: Image,
-  generator: z.string().optional(),
-  lastBuildDate: z.string().optional(),
-  "atom:link": AtomLink,
-  author: z.string().optional(),
-  copyright: z.string(),
-  language: z.string(),
-  "itunes:author": z.string(),
-  "itunes:summary": z.string().optional(),
-  "itunes:type": z.string(),
-  "itunes:owner": ItunesOwner,
-  "itunes:explicit": z.union([z.string(), z.boolean()]),
-  "itunes:category": ItunesCategory,
-  "itunes:image": ItunesImage,
-  item: z.array(Item),
-});
+	title: z.string(),
+	description: z.string(),
+	link: z.string(),
+	image: Image,
+	generator: z.string().optional(),
+	lastBuildDate: z.string().optional(),
+	'atom:link': AtomLink,
+	author: z.string().optional(),
+	copyright: z.string(),
+	language: z.string(),
+	'itunes:author': z.string(),
+	'itunes:summary': z.string().optional(),
+	'itunes:type': z.string(),
+	'itunes:owner': ItunesOwner,
+	'itunes:explicit': z.union([z.string(), z.boolean()]),
+	'itunes:category': ItunesCategory,
+	'itunes:image': ItunesImage,
+	item: z.array(Item),
+})
 
 const PodcastRSSFeed = z.object({
-  rss: z.object({
-    channel: Channel,
-  }),
-});
+	rss: z.object({
+		channel: Channel,
+	}),
+})
 
-export { PodcastRSSFeed };
+export default PodcastRSSFeed
--- src/types/spotifyEpisodes.ts
+++ src/types/spotifyEpisodes.ts
@@ -1,90 +1,90 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 // Define the schema for an image object
 const imageSchema = z.object({
-  url: z.string(),
-  height: z.number().nullable(),
-  width: z.number().nullable(),
-});
+	url: z.string(),
+	height: z.number().nullable(),
+	width: z.number().nullable(),
+})
 
 // Define the schema for a copyright object
 const copyrightSchema = z.object({
-  text: z.string(),
-  type: z.string(),
-});
+	text: z.string(),
+	type: z.string(),
+})
 
 // Define the schema for an external URL object
 const externalUrlsSchema = z.object({
-  spotify: z.string(),
-});
+	spotify: z.string(),
+})
 
 // Define the schema for a resume point object
 const resumePointSchema = z.object({
-  fully_played: z.boolean(),
-  resume_position_ms: z.number(),
-});
+	fully_played: z.boolean(),
+	resume_position_ms: z.number(),
+})
 
 // Define the schema for a restriction object
 const restrictionSchema = z.object({
-  reason: z.string(),
-});
+	reason: z.string(),
+})
 
 // Define the schema for an episode object
 const episodeSchema = z.object({
-  audio_preview_url: z.string().nullable(),
-  description: z.string(),
-  html_description: z.string(),
-  duration_ms: z.number(),
-  explicit: z.boolean(),
-  external_urls: externalUrlsSchema,
-  href: z.string(),
-  id: z.string(),
-  images: z.array(imageSchema),
-  is_externally_hosted: z.boolean(),
-  is_playable: z.boolean(),
-  language: z.string(),
-  languages: z.array(z.string()),
-  name: z.string(),
-  release_date: z.string(),
-  release_date_precision: z.string(),
-  resume_point: resumePointSchema.optional(),
-  type: z.string(),
-  uri: z.string(),
-  restrictions: restrictionSchema.optional(),
-});
+	audio_preview_url: z.string().nullable(),
+	description: z.string(),
+	html_description: z.string(),
+	duration_ms: z.number(),
+	explicit: z.boolean(),
+	external_urls: externalUrlsSchema,
+	href: z.string(),
+	id: z.string(),
+	images: z.array(imageSchema),
+	is_externally_hosted: z.boolean(),
+	is_playable: z.boolean(),
+	language: z.string(),
+	languages: z.array(z.string()),
+	name: z.string(),
+	release_date: z.string(),
+	release_date_precision: z.string(),
+	resume_point: resumePointSchema.optional(),
+	type: z.string(),
+	uri: z.string(),
+	restrictions: restrictionSchema.optional(),
+})
 
 // Define the schema for the episodes response object
 const episodesResponseSchema = z.object({
-  href: z.string(),
-  limit: z.number(),
-  next: z.string().nullable(),
-  offset: z.number(),
-  previous: z.string().nullable(),
-  total: z.number(),
-  items: z.array(episodeSchema),
-});
+	href: z.string(),
+	limit: z.number(),
+	next: z.string().nullable(),
+	offset: z.number(),
+	previous: z.string().nullable(),
+	total: z.number(),
+	items: z.array(episodeSchema),
+})
 
 // Define the schema for the main response object
 const EpisodesResponseSchema = z.object({
-  available_markets: z.array(z.string()),
-  copyrights: z.array(copyrightSchema),
-  description: z.string(),
-  html_description: z.string(),
-  explicit: z.boolean(),
-  external_urls: externalUrlsSchema,
-  href: z.string(),
-  id: z.string(),
-  images: z.array(imageSchema),
-  is_externally_hosted: z.boolean(),
-  languages: z.array(z.string()),
-  media_type: z.string(),
-  name: z.string(),
-  publisher: z.string(),
-  type: z.string(),
-  uri: z.string(),
-  total_episodes: z.number(),
-  episodes: episodesResponseSchema,
-});
+	available_markets: z.array(z.string()),
+	copyrights: z.array(copyrightSchema),
+	description: z.string(),
+	html_description: z.string(),
+	explicit: z.boolean(),
+	external_urls: externalUrlsSchema,
+	href: z.string(),
+	id: z.string(),
+	images: z.array(imageSchema),
+	is_externally_hosted: z.boolean(),
+	languages: z.array(z.string()),
+	media_type: z.string(),
+	name: z.string(),
+	publisher: z.string(),
+	type: z.string(),
+	uri: z.string(),
+	total_episodes: z.number(),
+	episodes: episodesResponseSchema,
+})
 
-type Episode = z.infer<typeof episodeSchema>;
-export { EpisodesResponseSchema, type Episode };
+type Episode = z.infer<typeof episodeSchema>
+export { EpisodesResponseSchema, type Episode }
