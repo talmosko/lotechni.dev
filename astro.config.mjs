165f8f11c5f5ecd4812fb97aa8adab1cdd459718
MeirJC <meir84@gmail.com>
2024-12-24 17:29:18 +0200

    setup components dir structure, updated import aliases and added Astro types fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config'
-
 import react from '@astrojs/react'
-
 import tailwind from '@astrojs/tailwind'
-
 import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
