d159ec6cb8d7831966c4cf4845d803b23638d903
Saar Davidson <davidsonsaar@gmail.com>
2025-02-10 17:34:03 +0200

    Refactor EpisodeCard to conditionally render Spotify embed or image

--- src/components/react/EpisodeCardDescription.tsx
+++ src/components/react/EpisodeCardDescription.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react';
 
 interface EpisodeCardDescriptionProps {
-  audio_preview_url?: string | null;
   description?: string;
   html_description?: string;
-  images: any[];
-  name: string;
 }
 
 export default function EpisodeCardDescription({ description, html_description }: EpisodeCardDescriptionProps) {
@@ -23,20 +20,8 @@ export default function EpisodeCardDescription({ description, html_description }
         className="mt-2 cursor-pointer flex items-center justify-between"
         onClick={() => setOpen(!open)}
       >
-        <span>{open && html_description ? <div className="mt-2 inline-block" dangerouslySetInnerHTML={{ __html: html_description }} /> : trimmedSummary}</span>
-        <svg
-          className={`h-5 w-5 transition-transform duration-200 self-start ${open ? 'rotate-180' : ''}`}
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M19 9l-7 7-7-7"
-          />
-        </svg>
+        {open && html_description ? <div className="mt-2 inline-block" dangerouslySetInnerHTML={{ __html: html_description }} /> : <span>{trimmedSummary}</span>}
+        <span className={`icon-[tabler--caret-down-filled] text-current min-w-8 self-start transition-transform duration-200 sticky top-24 ${open ? 'rotate-180' : ''}`}></span>
       </div>
     </>
   );
