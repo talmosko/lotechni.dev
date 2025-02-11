8a4538a85cda72b6dd987ac3263e9ce41f046860
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2025-02-12 01:20:12 +0200

    Add success stories data type and update SuccessStories component

--- src/data/pages-data/successStories.ts
+++ src/data/pages-data/successStories.ts
@@ -1,6 +1,7 @@
 import SuccessStoryImage1 from '@assets/images/success-stories/success-story-1.jpeg'
+import type { SuccessStory } from '@data/types/successStories'
 
-export const successStoriesData = [
+export const successStoriesData: SuccessStory[] = [
   { image: SuccessStoryImage1, title: 'success-story-1' },
   { image: SuccessStoryImage1, title: 'success-story-2' },
   { image: SuccessStoryImage1, title: 'success-story-3' },
