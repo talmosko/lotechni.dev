3bc18df7407bf7c0a31af2b17b6dc15ef650e028
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2025-02-12 00:59:51 +0200

    Refactor success stories: Remove unused images and update data structure for the success-image
    also modified the ImageContainer to bring it back to it's original design and created new component for the usage of the grid `GridImageContainer`

--- src/data/pages-data/success-stories.ts
+++ src/data/pages-data/success-stories.ts
@@ -1,37 +1,12 @@
-import SuccessStoryImage1 from '../../assets/images/success-stories/success-story-1.jpeg'
-import SuccessStoryImage2 from '../../assets/images/success-stories/success-story-2.jpeg'
-import SuccessStoryImage3 from '../../assets/images/success-stories/success-story-3.jpeg'
-import SuccessStoryImage4 from '../../assets/images/success-stories/success-story-4.jpeg'
-import SuccessStoryImage5 from '../../assets/images/success-stories/success-story-5.jpeg'
-import SuccessStoryImage6 from '../../assets/images/success-stories/success-story-6.jpeg'
-import SuccessStoryImage7 from '../../assets/images/success-stories/success-story-7.jpeg'
-import SuccessStoryImage8 from '../../assets/images/success-stories/success-story-8.jpeg'
-import SuccessStoryImage9 from '../../assets/images/success-stories/success-story-9.jpeg'
-import SuccessStoryImage10 from '../../assets/images/success-stories/success-story-10.jpeg'
-import SuccessStoryImage11 from '../../assets/images/success-stories/success-story-11.jpeg'
-import SuccessStoryImage12 from '../../assets/images/success-stories/success-story-12.jpeg'
-import SuccessStoryImage13 from '../../assets/images/success-stories/success-story-13.jpeg'
-import SuccessStoryImage14 from '../../assets/images/success-stories/success-story-14.jpeg'
-import SuccessStoryImage15 from '../../assets/images/success-stories/success-story-15.jpeg'
-import SuccessStoryImage16 from '../../assets/images/success-stories/success-story-16.jpeg'
-import SuccessStoryImage17 from '../../assets/images/success-stories/success-story-17.jpeg'
+import SuccessStoryImage1 from '@assets/images/success-stories/success-story-1.jpeg'
 
-export const successStoriesImages = [
-  SuccessStoryImage1,
-  SuccessStoryImage2,
-  SuccessStoryImage3,
-  SuccessStoryImage4,
-  SuccessStoryImage5,
-  SuccessStoryImage6,
-  SuccessStoryImage7,
-  SuccessStoryImage8,
-  SuccessStoryImage9,
-  SuccessStoryImage10,
-  SuccessStoryImage11,
-  SuccessStoryImage12,
-  SuccessStoryImage13,
-  SuccessStoryImage14,
-  SuccessStoryImage15,
-  SuccessStoryImage16,
-  SuccessStoryImage17,
+export const successStoriesData = [
+  { image: SuccessStoryImage1, title: 'success-story-1' },
+  { image: SuccessStoryImage1, title: 'success-story-2' },
+  { image: SuccessStoryImage1, title: 'success-story-3' },
+  { image: SuccessStoryImage1, title: 'success-story-4' },
+  { image: SuccessStoryImage1, title: 'success-story-5' },
+  { image: SuccessStoryImage1, title: 'success-story-6' },
+  { image: SuccessStoryImage1, title: 'success-story-7' },
+  { image: SuccessStoryImage1, title: 'success-story-8' },
 ]
