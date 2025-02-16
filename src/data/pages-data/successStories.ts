ade804b87d10d3263e05b5a98f47cf8d6d21f441
talmosko <tal.moskovich@gmail.com>
2025-02-16 08:28:39 +0200

    Update success stories images and data
    - Replace old PNG success story images with new JPG images
    - Expand success stories data from 8 to 27 unique entries
    - Remove old success story image imports

--- src/data/pages-data/successStories.ts
+++ src/data/pages-data/successStories.ts
@@ -1,16 +1,59 @@
-import SuccessStoryImage1 from '@assets/images/success-stories/success-story-1.png'
-import SuccessStoryImage2 from '@assets/images/success-stories/success-story-2.png'
-import SuccessStoryImage3 from '@assets/images/success-stories/success-story-3.png'
+import Success1 from '@assets/images/success-stories/success1.jpg'
+import Success2 from '@assets/images/success-stories/success2.jpg'
+import Success3 from '@assets/images/success-stories/success3.jpg'
+import Success4 from '@assets/images/success-stories/success4.jpg'
+import Success5 from '@assets/images/success-stories/success5.jpg'
+import Success6 from '@assets/images/success-stories/success6.jpg'
+import Success7 from '@assets/images/success-stories/success7.jpg'
+import Success8 from '@assets/images/success-stories/success8.jpg'
+import Success9 from '@assets/images/success-stories/success9.jpg'
+import Success10 from '@assets/images/success-stories/success10.jpg'
+import Success11 from '@assets/images/success-stories/success11.jpg'
+import Success12 from '@assets/images/success-stories/success12.jpg'
+import Success13 from '@assets/images/success-stories/success13.jpg'
+import Success14 from '@assets/images/success-stories/success14.jpg'
+import Success15 from '@assets/images/success-stories/success15.jpg'
+import Success16 from '@assets/images/success-stories/success16.jpg'
+import Success17 from '@assets/images/success-stories/success17.jpg'
+import Success18 from '@assets/images/success-stories/success18.jpg'
+import Success19 from '@assets/images/success-stories/success19.jpg'
+import Success20 from '@assets/images/success-stories/success20.jpg'
+import Success21 from '@assets/images/success-stories/success21.jpg'
+import Success22 from '@assets/images/success-stories/success22.jpg'
+import Success23 from '@assets/images/success-stories/success23.jpg'
+import Success24 from '@assets/images/success-stories/success24.jpg'
+import Success25 from '@assets/images/success-stories/success25.jpg'
+import Success26 from '@assets/images/success-stories/success26.jpg'
+import Success27 from '@assets/images/success-stories/success27.jpg'
 
 import type { SuccessStory } from '@data/types/successStories'
 
 export const successStoriesData: SuccessStory[] = [
-  { image: SuccessStoryImage1, title: 'success-story-1' },
-  { image: SuccessStoryImage2, title: 'success-story-2' },
-  { image: SuccessStoryImage3, title: 'success-story-3' },
-  { image: SuccessStoryImage3, title: 'success-story-4' },
-  { image: SuccessStoryImage1, title: 'success-story-5' },
-  { image: SuccessStoryImage1, title: 'success-story-6' },
-  { image: SuccessStoryImage2, title: 'success-story-7' },
-  { image: SuccessStoryImage2, title: 'success-story-8' },
+  { image: Success1, title: 'success-story-1' },
+  { image: Success2, title: 'success-story-2' },
+  { image: Success3, title: 'success-story-3' },
+  { image: Success4, title: 'success-story-4' },
+  { image: Success5, title: 'success-story-5' },
+  { image: Success6, title: 'success-story-6' },
+  { image: Success7, title: 'success-story-7' },
+  { image: Success8, title: 'success-story-8' },
+  { image: Success9, title: 'success-story-9' },
+  { image: Success10, title: 'success-story-10' },
+  { image: Success11, title: 'success-story-11' },
+  { image: Success12, title: 'success-story-12' },
+  { image: Success13, title: 'success-story-13' },
+  { image: Success14, title: 'success-story-14' },
+  { image: Success15, title: 'success-story-15' },
+  { image: Success16, title: 'success-story-16' },
+  { image: Success17, title: 'success-story-17' },
+  { image: Success18, title: 'success-story-18' },
+  { image: Success19, title: 'success-story-19' },
+  { image: Success20, title: 'success-story-20' },
+  { image: Success21, title: 'success-story-21' },
+  { image: Success22, title: 'success-story-22' },
+  { image: Success23, title: 'success-story-23' },
+  { image: Success24, title: 'success-story-24' },
+  { image: Success25, title: 'success-story-25' },
+  { image: Success26, title: 'success-story-26' },
+  { image: Success27, title: 'success-story-27' },
 ]
